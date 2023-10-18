df9f7224c39a14e5c0062892349aac5f0fb504a7
Li_Tian <Li_Tian@singaporeair.com.sg>
2023-10-18 20:20:24 +0800

    Add post feature
    notes:
    Components:  UI Components
    lib: shared functions
    blogpost: place the markdown files
    types.d.ts:  define structures
    
    close #4

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import Posts from './Components/Posts'
 
 export default function Home() {
   return (
     <main className='px-6 mx-auto'>
       <p className='mt-12 mb-12 text-3xl text-center dark:text-white'>Hello</p>
+      <Posts />
     </main>
   )
 }
