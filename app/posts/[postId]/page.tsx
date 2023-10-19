e3d43079c847f79a024664fade94e2ea9712bb8d
Li_Tian <Li_Tian@singaporeair.com.sg>
2023-10-19 11:56:58 +0800

    fixed #6 the Deploy failed type wrong name on generateMetadata
    [#6]

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 import { notFound } from "next/navigation"
 
 
-export function generateMetaData({ params }: { params: { postId: string } }) {
+export function generateMetadata({ params }: { params: { postId: string } }) {
     const posts = getSortedPostData()
     const { postId } = params
 
